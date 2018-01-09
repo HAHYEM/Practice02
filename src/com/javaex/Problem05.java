acc174c97ae6994cac95150bbc6ba5f7e3afbf38
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 13:08:50 +0900

    5수정

--- src/com/javaex/Problem05.java
+++ src/com/javaex/Problem05.java
@@ -2,27 +2,23 @@ package com.javaex;
 
 public class Problem05 {
 	public static void main(String[] args) {
-		
 
-int[] random = new int[6];
-int temp = 0;
+		int[] random = new int[6];
+		int temp = 0;
 
-for (int i = 0; i < 6; i++) {
-	temp = (int) ((Math.random() * 45) + 1);
-	random[i] = temp;
-
-	for (int j = 0; j < i; j++) {
-		if (random[i] == random[j]) {
+		for (int i = 0; i < 6; i++) {
 			temp = (int) ((Math.random() * 45) + 1);
 			random[i] = temp;
-			temp -= 1;
-			break;
+
+			for (int j = 0; j < i; j++) {
+				if (random[i] == random[j]) {
+					temp = (int) ((Math.random() * 45) + 1);
+					random[i] = temp;
+					j=0;
+					break;
+				}
+			}
+			System.out.print(random[i] + "   ");
 		}
-	}System.out.print(random[i]+"   ");
-}
-
-
-
-}
-
+	}
 }

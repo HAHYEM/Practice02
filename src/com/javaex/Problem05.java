17a434546126f94cd852afc48f6fe27482a2ff58
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 09:04:32 +0900

    5번 수정

--- src/com/javaex/Problem05.java
+++ src/com/javaex/Problem05.java
@@ -3,11 +3,26 @@ package com.javaex;
 public class Problem05 {
 	public static void main(String[] args) {
 		
-		int random [] = new int[6];
-	for(int i = 0; i < random.length; i++) {
-		random[i] = (int) (Math.random() * 45 + 1);
-		System.out.print(random[i] + "   ");
-	}
-	
-	}
+
+int[] random = new int[6];
+int temp = 0;
+
+for (int i = 0; i < 6; i++) {
+	temp = (int) ((Math.random() * 45) + 1);
+	random[i] = temp;
+
+	for (int j = 0; j < i; j++) {
+		if (random[i] == random[j]) {
+			temp = (int) ((Math.random() * 45) + 1);
+			random[i] = temp;
+			temp -= 1;
+			break;
+		}
+	}System.out.print(random[i]+"   ");
+}
+
+
+
+}
+
 }
